aade61300d69cd02f9a6a4cf17e861323176677a
hamza.ghazli <hghazli56@gmail.com>
2022-06-12 09:39:48 +0100

    .sh edit

--- dbprovision.sh
+++ dbprovision.sh
@@ -1,25 +1,27 @@
+#!/bin/bash
+
+#Update, install MongoDB and upgrade
 sudo apt-get update -y
-
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
-
 echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-
 sudo apt-get update -y
 sudo apt-get upgrade -y
-
-# sudo apt-get install mongodb-org=3.2.20 -y
-
+#sudo apt-get install mongodb-org=3.2.20 -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+#If mongo is is set up correctly these will be successful
+sudo systemctl restart mongod
+sudo systemctl enable mongod
+#sudo mv mongod.conf /etc/mongod.conf
 
-# if mongo is is set up correctly these will be successful
+
+#Edit mongod.conf to replace 127.0.0.1 with 0.0.0.0 so that port 27017 is open on whatever ip the database VM is assigned
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+
+#After mongod.conf file is changed, we need to restart and enable mongodb so new configurations are in effect
 sudo systemctl restart mongod
 sudo systemctl enable mongod
 
-sudo mv mongod.conf /etc/mongod.conf
-
-sudo systemctl restart mongod
-sudo systemctl enable mongod
-# After mongod.conf file is changed, we need to restart and enable mongodb so new confgiuations are in effect
 
 
 
--- user_data_app.sh
+++ user_data_app.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
-# update
+# Update and upgrade 
 sudo apt-get update -y
-# upgrade
 sudo apt-get upgrade -y
-# install nginx
+
+# Install nginx
 sudo apt-get install nginx -y
-# start nginx
 sudo systemctl start nginx
-# enable nginx
 sudo systemctl enable nginx
-# install dependencies
+
+# Install dependencies
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-
 sudo apt-get install nodejs -y
-
 sudo apt-get install npm 
-
 sudo npm install pm2 -g
-
 sudo apt-get install python-software-properties -y
 
+# Copy codebase from Github repo
 mkdir repo
-
 cd repo
-
 git clone https://github.com/hghazli56/eng114_devops.git
-
 cd eng114_devops/
 
+# Copy premade default file to nginx configuration 
+# This will forward the applications original port(3000) to the servers defult port(80) 
 sudo mv default /etc/nginx/sites-available/default 
 
+# Restart nginx for configuration to take effect 
 sudo systemctl restart nginx
 
+# Next we create the environment variable DB_HOST in order for our app to connect to the database through its ip on port 27017
 sudo echo "export DB_HOST='mongodb://3.250.139.104:27017/posts'" >> /etc/bash.bashrc 
-
 source /etc/bash.bashrc
 
 sudo apt npm install
